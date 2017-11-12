20c0cc40f2a63c13b03b2630a4090af396d24f72
Luke Stewart <lukestewart@gmail.com>
2017-11-12 13:18:27 -0700

    changes

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+//STAR WARS RPG
+
+//Create objects for the 4 different characters, HP, Attack Power, Counter Attack Power
+
+//Create a "hero" class or something that remembers which character the player chooses
+
+//Create a defender area that remembers which enemy the player is attacking
+
+//Create an Attack Power multiplier function or loop
+
+//Create a function that "fights" the two opponents, deleting HP frome each, killing one off eventually
+
+
+
